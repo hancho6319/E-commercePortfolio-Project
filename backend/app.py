781a79012bbe52f2c9204b4bddbf317939a9d4e5
hancho6319 <abbaauwalharuna@gmail.com>
2024-04-30 05:52:07 +0000

    this is a commit for update button

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,43 @@
 from flask import Flask, render_template, request
+import mysql.connector
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def method_name():
-    user_agent = request.user_agent.string
+# MySQL connection configuration
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="hancho6319",
+    password="JKX#3aZ441vid",
+    database="E-commercePortfolio-Project"
+)
 
-    # Check if the User-Agent string contains keywords indicative of a mobile device
-    is_mobile = 'Mobi' in user_agent or 'Android' in user_agent or 'iOS' in user_agent
-    if is_mobile:
-        return render_template('index.html', mode="mobile")
-    else:
-        return render_template('index.html', mode="desktop")
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=81)
+@app.route('/')
+def index():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    surname = request.form['surname']
+    other_name = request.form['other_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    phone_number = request.form['phone_number']
+    created_time_date = datetime.now()
+    product_id = request.form['product_id']
+    nationality = request.form['nationality']
+    state_province = request.form['state_province']
+    address = request.form['address']
+
+    cursor = mydb.cursor()
+    sql = "INSERT INTO customers (surname, other_name, last_name, email, phone_number, created_time_date, product_id, nationality, state_province, address) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (surname, other_name, last_name, email, phone_number, created_time_date, product_id, nationality, state_province, address)
+    cursor.execute(sql, val)
+    mydb.commit()
+    cursor.close()
+
+    return 'Data successfully submitted to MySQL'
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=81)
+
